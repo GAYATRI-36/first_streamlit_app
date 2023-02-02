50b398928a08713f13579160a2b793d0e1483be5
GAYATRI-36 <124164032+GAYATRI-36@users.noreply.github.com>
2023-02-02 12:21:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-mu_fruit_list = my_fruit_list.set_index( 'fruit' )
+my_fruit_list = my_fruit_list.set_index( 'fruit' )
 
